b99398d99b94e37d023b91b69a308f66ab8c277e
Ennas Adi <ennasaadi523@gmail.com>
2026-06-24 22:16:08 +0300

    Add ensure_superuser command and update build

---
+++ store/management/commands/ensure_superuser.py
@@ -0,0 +1,28 @@
+﻿import os
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Create or update a superuser from DJANGO_SUPERUSER_* environment variables."
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+        email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "")
+
+        if not username or not password:
+            self.stdout.write("DJANGO_SUPERUSER_USERNAME / DJANGO_SUPERUSER_PASSWORD not set - skipping.")
+            return
+
+        user, created = User.objects.get_or_create(username=username, defaults={"email": email})
+        if email:
+            user.email = email
+        user.is_staff = True
+        user.is_superuser = True
+        user.set_password(password)
+        user.save()
+
+        action = "Created" if created else "Updated"
+        self.stdout.write(self.style.SUCCESS(f"{action} superuser " + username))
